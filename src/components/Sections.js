2b69d4380c935946e7a86d9fd0ad654a35942a32
Minh To <toanhminh0412@gmail.com>
2023-04-03 05:31:48 -0700

    Latest update

--- src/components/Sections.js
+++ src/components/Sections.js
@@ -317,7 +317,6 @@ export function Testimonials() {
     'job': 'Software engineer in Canada'}
   ])
   const [reviewShown, setReviewShown] = useState(0);
-  console.log(reviews);
 
   return (
     <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
--- src/components/upperNav.js
+++ src/components/upperNav.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Link from 'next/link'
 import {BiMenu} from "react-icons/bi";
 
 export default function UpperNav({active="home"}) {
@@ -9,10 +10,10 @@ export default function UpperNav({active="home"}) {
       <div className="px-2 py-3 lg:px-20 flex flex-row justify-between">
         <h1 className="text-white text-xl lg:text-3xl font-medium">ForexCopyTrade</h1>
         <div className="hidden lg:flex lg:flex-row">
-          <a href="/" className={`text-white ${active === "home" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl my-auto mx-4`}>Home</a>
-          <a href="/about" className={`text-white ${active === "about" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl my-auto mx-4`}>About</a>
-          <a href="/services" className={`text-white ${active === "services" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl my-auto mx-4`}>Services</a>
-          <a href="/contact-us" className={`text-white ${active === "contact-us" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl my-auto mx-4`}>Contact us</a>
+          <Link href="/" className={`text-white ${active === "home" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl my-auto mx-4`}>Home</Link>
+          <Link href="/about" className={`text-white ${active === "about" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl my-auto mx-4`}>About</Link>
+          <Link href="/services" className={`text-white ${active === "services" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl my-auto mx-4`}>Services</Link>
+          <Link href="/contact-us" className={`text-white ${active === "contact-us" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl my-auto mx-4`}>Contact us</Link>
         </div>
         <BiMenu size={30} className={`${smMenuOpened ? 'text-violet-200': 'text-white'} hover:text-violet-200 font-normal text-2xl my-auto lg:hidden`} onClick={() => {setSmMenuOpened(!smMenuOpened)}}/>
       </div>
@@ -28,16 +29,16 @@ function SmMenu({opened=false, active="home"}) {
     return (
       <div className="bg-violet-500 lg:hidden fixed top-14 left-0 z-40 w-screen">
         <div className="py-2 border-b border-violet-700">
-          <a href="/" className={`text-white ${active === "home" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl mx-4`}>Home</a>
+          <Link href="/" className={`text-white ${active === "home" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl mx-4`}>Home</Link>
         </div>
         <div className="py-2 border-b border-violet-700">
-          <a href="/about" className={`text-white ${active === "about" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl mx-4`}>About</a>
+          <Link href="/about" className={`text-white ${active === "about" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl mx-4`}>About</Link>
         </div>
         <div className="py-2 border-b border-violet-700">
-          <a href="/services" className={`text-white ${active === "services" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl mx-4`}>Services</a>
+          <Link href="/services" className={`text-white ${active === "services" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl mx-4`}>Services</Link>
         </div>
         <div className="py-2 border-b border-violet-700">
-          <a href="/contact-us" className={`text-white ${active === "contact-us" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl mx-4`}>Contact us</a>
+          <Link href="/contact-us" className={`text-white ${active === "contact-us" ? 'font-normal hover:font-semibold' : 'font-light hover:font-normal'} text-lg lg:text-xl mx-4`}>Contact us</Link>
         </div>
       </div>
     )

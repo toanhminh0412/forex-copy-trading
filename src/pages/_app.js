b5610d74069303a9ea47925c47394f099e0efd78
Archie To <archieto@Archies-MacBook-Pro.local>
2023-05-04 07:42:57 -0700

    Enable app analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
+import { Analytics } from '@vercel/analytics/react';
 import '@/styles/globals.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics/>
+    </>
+  )
 }
